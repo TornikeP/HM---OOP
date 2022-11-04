78ed525c4c04fe4b183d1d2a2e30a081225fa166
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-11-04 19:41:50 +0400

    .

---
+++ script.js
@@ -0,0 +1,49 @@
+alert ("ამით თუ არ დავიწყე რაა ჯავასკრიპტი")
+
+function Car(cMake, cModel, cYear) {
+    this.make = cMake;
+    this.model = cModel;
+    this.year = cYear;
+    this.getCarInfo = function() {
+        document.write("სახელი" + this.make + "მოდელი" + this.model + "გამოშვების წელი" + this.year)
+    }   
+}
+
+function Person (pName, pAge, pCars ) {
+    this.name = pName;
+    this.age = pAge;
+    this.cars = pCars;
+    this.driveCar = function (car) {
+        document.write(" " + "he" + " " + "has" + " " + "a" + " " + car.make + " " + car.model + " " + car.year)
+    }
+       this.displayInfo = function () {
+           document.write (this.name + " " + "is" + " " + this.age + " " + "years" +" " +  "old" + "," )
+       }
+}
+
+var tom = new Person ("Tom", 24)
+tom.displayInfo()
+
+var cr = new Car ("Tesla", "Model S", 2015)
+tom.driveCar(cr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
